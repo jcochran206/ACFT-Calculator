665faf06528f19b5e951d981e853a1efb404bcc2
jcochran206 <jonathan.o.cochran@gmail.com>
2025-05-27 23:54:34 -0700

    added SPTF21 data

--- app.js
+++ app.js
@@ -75,6 +75,52 @@ const MDLM21 = [
     {weight: "80", Score: "0"},
 ]
 
+const SPTF21 = [
+    {meters: "8.4", Score: "100"},
+    {meters: "8.2", Score: "99"},
+    {meters: "7.7", Score: "98"},
+    {meters: "7.5", Score: "97"},
+    {meters: "7.3", Score: "96"},
+    {meters: "7.2", Score: "95"},
+    {meters: "7.0", Score: "94"},
+    {meters: "6.9", Score: "93"},
+    {meters: "6.8", Score: "92"},
+    {meters: "6.6", Score: "91"},
+    {meters: "6.4", Score: "88"},
+    {meters: "6.3", Score: "87"},
+    {meters: "6.2", Score: "86"},
+    {meters: "6.1", Score: "65"},
+    {meters: "6.0", Score: "83"},
+    {meters: "5.9", Score: "81"},
+    {meters: "5.8", Score: "80"},
+    {meters: "5.7", Score: "78"},
+    {meters: "5.6", Score: "76"},
+    {meters: "5.5", Score: "75"},
+    {meters: "5.4", Score: "74"},
+    {meters: "5.3", Score: "72"},
+    {meters: "5.2", Score: "71"},
+    {meters: "5.1", Score: "69"},
+    {meters: "5.0", Score: "68"},
+    {meters: "4.9", Score: "66"},
+    {meters: "4.8", Score: "65"},
+    {meters: "4.7", Score: "63"},
+    {meters: "4.6", Score: "62"},
+    {meters: "4.4", Score: "61"},
+    {meters: "3.9", Score: "60"},
+    {meters: "3.8", Score: "54"},
+    {meters: "3.7", Score: "44"},
+    {meters: "3.6", Score: "37"},
+    {meters: "3.5", Score: "32"},
+    {meters: "3.4", Score: "24"},
+    {meters: "3.3", Score: "18"},
+    {meters: "3.2", Score: "14"},
+    {meters: "3.1", Score: "11"},
+    {meters: "3.0", Score: "8"},
+    {meters: "2.9", Score: "6"},
+    {meters: "2.8", Score: "4"},
+    {meters: "2.7", Score: "2"},
+    {meters: "2.6", Score: "0"},
+]
 
 
 //set inital values (mdl, spt, hrp, 2mr)
@@ -114,7 +160,7 @@ navLinks.forEach(item => {
     ulElement.appendChild(liElement);
 });
 
-// Button click handlers
+// eventlisteners button click handlers
 femaleBtn.addEventListener('click', () => {
     selectedGender = 'female';
     updateMDLScoreDisplay(parseInt(mdlSlider.value));
